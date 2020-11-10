a884a69ca51a176c3a16094a3dad5d0435105cb9
mwas <emmamwas99@gmail.com>
2020-11-10 05:33:55 +0300

    added asyc/await to consume multiple promises

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const readFilePromise = (file) => {
     });
   });
 };
-
 const writeFilePromise = (file, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(file, data, (err) => {
@@ -19,17 +18,40 @@ const writeFilePromise = (file, data) => {
   });
 };
 
-readFilePromise(`${__dirname}/dog.txt`)
-  .then((data) => {
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
-    return writeFilePromise("new-dog.txt", res.body.message);
-  })
-  .then(() => {
-    console.log("data written successfuly");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const getDog = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log(` Breed: ${data}`);
+    const res1 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res4 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1, res2, res3, res4]);
+    const images = all.map((el) => el.body.message);
+    await writeFilePromise("new-dog.txt", images.join("\n\n"));
+    console.log("file written");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+  return "Everything went Ok";
+};
+(async () => {
+  try {
+    console.log("1: Phase one of getting data");
+    console.log("2: Phase two of getting data");
+    const x = await getDog();
+    console.log(x);
+  } catch (error) {
+    console.log("Error found");
+  }
+})();
